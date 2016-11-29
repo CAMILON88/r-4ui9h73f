4bd453d8e8eb826a75d185d123fe2f13a4151380
German Escobar <german.escobarc@gmail.com>
2016-11-29 02:16:54 -0500

    Prueba arreglada y actualizada

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+
+    super()
+    this.state = {
+
+
+      text: ""
+    }
+  }
+  textChange (event) {
+    this.setState({ text: event.target.value })
+
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Un repetidor</h1>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.textChange.bind(this)} />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
